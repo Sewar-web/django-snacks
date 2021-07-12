27d63a186e4b93e408797086fbd91c660a0a540d
Sewar-web <s.maqableh@yahoo.com>
2021-07-12 16:12:45 +0300

    test for template

--- snacks/tests.py
+++ snacks/tests.py
@@ -14,3 +14,15 @@ class MoviesTests(SimpleTestCase):
         actual_status_code = self.client.get(url).status_code
         expected_status_code = 200
         self.assertEqual(actual_status_code, expected_status_code)
+
+    def test_home_page_template(self):
+        url = reverse('home')
+        actual_status_code = self.client.get(url)
+        expected_status_code = 'home.html'
+        self.assertTemplateUsed(actual_status_code, expected_status_code)
+
+    def test_about_page_template(self):
+        url = reverse('about')
+        actual_status_code = self.client.get(url)
+        expected_status_code = 'about.html'
+        self.assertTemplateUsed(actual_status_code, expected_status_code)
